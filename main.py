46f53ce2cdb81862fe5787cb10b58fa411b4e75c
luccondevaux-cpu <luccondevaux@gmail.com>
2026-03-13 06:41:02 +0000

    Finished Polynomial

--- main.py
+++ main.py
@@ -38,24 +38,47 @@ def polynomial_magic(list, next):
 
 def polynomial_magic(list):
     triangle, diag = polynomial_magic_stats(list)
-    return diag_processing(diag)
+    return diag_processing(diag, True)
 
 
-def diag_processing(diag):
+def diag_processing(diag, first):
     length = len(diag)
     print("length", length)
+    if length == 0:
+        return ""
 
     if length == 1:
-        return diag[0]
+        return str(diag[0])
     
     pow_diag_end = 1
-    for i in range(length-1):#length 4, pow 3, pow_diag_end = 6
+    for i in range(length-1):#length 4, pow 3, pow_diag_end = 6 is an example
         pow_diag_end *= (i + 1)
     diag_end = diag[-1]/pow_diag_end
+    ignore, subt_diag = polynomial_magic_stats(x_pow_n(range(length), length-1))
+    diag = [a - (b*diag_end) for a,b in zip(diag, subt_diag)]
+    print("diag_subt_multiple(diag_end)", diag_end)
 
-    #change diag to subtract everything.
+    looping = True
+    while looping:
+        print("diag zero lowering", diag)
+        removed = diag.pop(-1)
+        print(len(diag))
+        if len(diag) == 0 and diag_end == 0:
+            return ""
+        elif len(diag) == 0:
+            looping = False
+        elif diag[-1] != 0:
+            looping = False
+    #what to do if you remove a diag value and everything after that is 0? the return is just nothing
+    #why return nothing; cause then it will get mad cause it will try to do above with nothing in diag. when diag is done, check values.
 
-    return diag_end, diag_processing(diag)
+    print("updated diag", diag)
+
+    print(first)
+    if first:
+        return str(diag_end) + "*x^" + str(length-1) + diag_processing(diag, not first)
+    first = True
+    return "+" + str(diag_end) + "*x^" + str(length-1) + diag_processing(diag, not first)
 
 
 def n_pow_x(n, x):#replaces any numbers with n^the number unless there are 2 or more dimensions
@@ -63,8 +86,13 @@ def n_pow_x(n, x):#replaces any numbers with n^the number unless there are 2 or 
         return n**x
     return [n**num for num in x]
 
+def x_pow_n(x, n):#replaces any numbers with the number^n unless there are 2 or more dimensions
+    if type(x) == int:
+        return x**n
+    return [num**n for num in x]
+
 def combine_lists(A, B):
     return[a + b for a,b in zip(A, B)]
 
-#polynomial_magic([num**4 for num in range(6)])#y=x^4
-print(polynomial_magic(combine_lists(n_pow_x(2.5, range(4)), n_pow_x(1.5, range(4))))#y=2.5^x+1.5^x)
+#Approximates the function describing the trend of a list of numbers such that the function is a polynomial of order/degree one less than the length of the list entered
+print(polynomial_magic(x_pow_n(range(5), 5)))
